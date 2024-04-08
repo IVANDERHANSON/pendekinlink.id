40c28b6b1e3924e412b6af5d1dbb279e21260189
hansonivander <hansonivander@gmail.com>
2024-04-08 23:41:24 +0700

    feat: [BE] Main Shorten Link

---
+++ app/Http/Controllers/CustomizedLinkController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CustomizedLink;
+use App\Http\Requests\StoreCustomizedLinkRequest;
+use App\Http\Requests\UpdateCustomizedLinkRequest;
+use Illuminate\Http\Request;
+
+class CustomizedLinkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'Link' => ['required'],
+            'Source' => ['required']
+        ]);
+
+        CustomizedLink::create([
+            'Link' => $request->Link,
+            'Source' => $request->Source
+        ]);
+
+        return redirect('/create');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CustomizedLink $customizedLink)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CustomizedLink $customizedLink)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCustomizedLinkRequest $request, CustomizedLink $customizedLink)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CustomizedLink $customizedLink)
+    {
+        //
+    }
+    
+    public function redirect($link) {
+        $Link = CustomizedLink::where('Link', $link)->first();
+        return redirect($Link->Source);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomizedLinkController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +30,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/create', [CustomizedLinkController::class, 'create']);
+Route::post('/store', [CustomizedLinkController::class, 'store']);
+Route::get('/{link}', [CustomizedLinkController::class, 'redirect']);
