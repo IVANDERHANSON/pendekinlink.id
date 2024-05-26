3ea11e36c85a0ffe42747e02c0341e184d064cc1
loyarr <ignatian1491@gmail.com>
2024-05-26 22:10:20 +0700

    feat: login&register, footer update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,30 +4,34 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'custom-blue': '#0075FF',
-        'custom-whitegrey': '#E3E3E3',
-        'custom-lightergrey': '#999999',
-        'custom-lightgrey': "#666666",
-        'custom-grey': '#2E2E2E',
-        'custom-black': '#111111'
+        "custom-blue": "#0075FF",
+        "custom-white": "#EFEFEF",
+        "custom-whitegrey": "#E3E3E3",
+        "custom-lightergrey": "#999999",
+        "custom-lightgrey": "#666666",
+        "custom-grey": "#2E2E2E",
+        "custom-black": "#111111",
       },
       fontFamily: {
-        body: ['Archivo']
+        body: ["Archivo"],
       },
       spacing: {
-        'custom-600px': '37.5rem',
+        "custom-600px": "37.5rem",
       },
       boxShadow: {
-        'custom': '2px 2px 6px rgba(0, 0, 0, 0.16)',
+        custom1: "2px 2px 6px rgba(0, 0, 0, 0.08)",
+        custom2: "2px 2px 6px rgba(0, 0, 0, 0.3)",
       },
       borderWidth: {
-        '3': '3px',
+        3: "3px",
+      },
+      borderRadius: {
+        "4xl": "32px",
       },
       backgroundImage: {
-        'gradient1': "url('/assets/gradient1.png')",
-      }
+        gradient1: "url('/assets/gradient1.png')",
+      },
     },
   },
   plugins: [],
-}
-
+};
