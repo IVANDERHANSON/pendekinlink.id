3020d9f59b6663bd16e56f26d913b0e9b1f50357
hansonivander <hansonivander@gmail.com>
2025-06-05 14:10:21 +0700

    feat: remove unused css

--- resources/views/editLink.blade.php
+++ resources/views/editLink.blade.php
@@ -3,7 +3,6 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="src/output.css" />
     <title>pendekinlink.id | Edit Link</title>
     <link rel="shortcut icon" href="{{ asset('assets/Logo.jpeg') }}" type="image/x-icon">
     @vite('resources/css/app.css')
